768c75ae6aa758a551d951546e71f783cd46c77c
nirbhayrana123 <nirbhydesign@gmail.com>
2023-02-02 16:08:57 +0530

    updated

---
+++ src/js/admin.js
@@ -0,0 +1,327 @@
+jQuery(document).ready(function () {
+  $(".slide").click(function () {
+    $(".dropdown").slideToggle();
+  });
+  $(".none").click(function () {
+    $(".dropdown").slideUp();
+  });
+
+  jQuery('#documents').DataTable();
+
+  $(".rightbut").click(function(){
+    $(".rightsidmenu").toggleClass("right");
+  });
+
+  $(".filtercu").click(function(){
+    $(".filtercustome").toggleClass("open");
+  });
+  $(".cancel").click(function(){
+    $(".filtercustome").removeClass("open");
+  });
+    $(".filternone").click(function(){
+      $(".filtercustome").removeClass("open");
+  });
+
+
+/////////////////jSn toggle//////////////////////////// 
+  $('input[type="checkbox"]#tableview').change(function() {
+    if ($(this).is(":checked")) { 
+      $(".viewjsn").hide();
+      $(".tableview").show(); 
+      $(".nocheked").text("Table View");
+      $(".nocheked").removeClass("text-[#4F4F4F]");
+
+    } else {
+      $(".nocheked").text("JSON View");
+      $(".nocheked").addClass("text-[#4F4F4F]");
+      $(".viewjsn").show();
+      $(".tableview").hide(); 
+  }
+}); 
+
+
+$(".viewbut").click(function(){  
+  if ($(this).hasClass('open')){
+    $('.viewbut').removeClass('open');
+  } else {
+    $('.viewbut').removeClass('open');
+    $(this).addClass('open');
+  }
+
+});
+
+
+
+
+$(".dropdown-content").click(function(){
+  $('.viewbut').removeClass('open');
+});
+
+
+///////////////////processes/////////
+$('.processes').click(function() {
+    $('.allprosess').removeClass('hidden');
+     $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+  $('.allprosess').addClass('hidden');
+   $('.all-listno').removeClass('hidden'); 
+});
+ ///////////////////processesend/////////
+
+ //////////////////dllsbut/////////////////
+
+$('.dllsbut').click(function() {
+  $('.dllsprosess').removeClass('hidden');
+   $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+$('.dllsprosess').addClass('hidden');
+ $('.all-listno').removeClass('hidden'); 
+});
+////////////////dllsbut end/////////////
+
+////////////Handleprocesses/////////////
+$('.Handleprocesses').click(function() {
+  $('.handlestabprosess').removeClass('hidden');
+   $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+$('.handlestabprosess').addClass('hidden');
+ $('.all-listno').removeClass('hidden'); 
+});
+//////////////////Handleprocesses end////////////////
+
+//////////////////Memoryprocesses //////////////////
+$('.Memoryprocesses').click(function() {
+  $('.memorytabprosess').removeClass('hidden');
+   $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+$('.memorytabprosess').addClass('hidden');
+ $('.all-listno').removeClass('hidden'); 
+});
+
+
+///////////////Memoryprocesses end///////////
+
+//////////////////Threadsprocesses //////////////////
+$('.Threadsprocesses').click(function() {
+  $('.threadstabprosess').removeClass('hidden');
+   $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+$('.threadstabprosess').addClass('hidden');
+ $('.all-listno').removeClass('hidden'); 
+});
+
+
+///////////////Threadsprocesses end///////////
+
+
+//////////////////Variablesprocesses Start //////////////////
+$('.Variablesprocesses').click(function() {
+  $('.Variablestabprosess').removeClass('hidden');
+   $('.all-listno').addClass('hidden'); 
+});
+$('.close-process').click(function() {
+$('.Variablestabprosess').addClass('hidden');
+ $('.all-listno').removeClass('hidden'); 
+});
+
+
+//////////////////Variablesprocesses End //////////////////
+
+
+
+
+
+
+
+
+
+
+////////////////////Drivers Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".driverstables").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".driverstables").hide(); 
+}
+}); 
+
+////////////////////Drivers End///////////////////////
+
+
+////////////////////Registry Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".registry").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".registry").hide(); 
+}
+}); 
+
+////////////////////Registry End///////////////////////
+
+////////////////////Syscalls Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".Syscalls").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".Syscalls").hide(); 
+}
+}); 
+
+////////////////////Syscalls End///////////////////////
+
+////////////////////Objects Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".Objects").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".Objects").hide(); 
+}
+}); 
+
+////////////////////Objects End///////////////////////
+
+////////////////////Sockets Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".Sockets").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".Sockets").hide(); 
+}
+}); 
+
+////////////////////Sockets End///////////////////////
+
+////////////////////Callbacks Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".Callbacks").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".Callbacks").hide(); 
+}
+}); 
+
+////////////////////Callbacks End///////////////////////
+
+////////////////////Scheduled Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".Scheduled").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".Scheduled").hide(); 
+}
+}); 
+
+////////////////////Scheduled End///////////////////////
+////////////////////autoruns Start///////////////////////
+
+$('input[type="checkbox"]#tableview').change(function() {
+  if ($(this).is(":checked")) { 
+    $(".viewjsn").hide();
+    $(".autoruns").show(); 
+    $(".nocheked").text("Table View");
+    $(".nocheked").removeClass("text-[#4F4F4F]");
+
+  } else {
+    $(".nocheked").text("JSON View");
+    $(".nocheked").addClass("text-[#4F4F4F]");
+    $(".viewjsn").show();
+    $(".autoruns").hide(); 
+}
+}); 
+
+////////////////////autoruns End///////////////////////
+
+
+
+////////////////////////////login//////////////////
+
+
+
+}); 
+$("#forgetpass").click(function(){
+  $("#mainlogin").addClass("hidden");
+  $("#sendlink").removeClass("hidden");
+});
+
+   //////////Dashboard////////////
+  
+
+   var sideBar = document.getElementById("mobile-nav");
+   var openSidebar = document.getElementById("openSideBar");
+   var closeSidebar = document.getElementById("closeSideBar");
+   sideBar.style.transform = "translateX(-260px)";
+
+  
+   function sidebarHandler(flag) {
+     if (flag) {
+       sideBar.style.transform = "translateX(0px)";
+       openSidebar.classList.add("hidden");
+       closeSidebar.classList.remove("hidden");
+     } else {
+       sideBar.style.transform = "translateX(-260px)";
+       closeSidebar.classList.add("hidden");
+       openSidebar.classList.remove("hidden");
+     }
+   }
---
+++ tailwind.config.js
@@ -0,0 +1,84 @@
+const { Container } = require('postcss');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      height: {
+        'sm': '50px',
+      },
+      colors: {
+        'shamrock':'rgba(60, 210, 165, 0.2)',
+        'white': '#fff',
+        'inputbg':'#D6F3FB', 
+        'blue' :'#114273',
+        'uplodecolor':'rgba(17, 66, 115, 0.05);',
+        'bron' : 'rgba(4, 140, 185, 0.1)',
+        'tan':'rgba(238, 190, 94, 0.2)',
+        'bluener':'rgba(130, 157, 227, 0.15)',
+        'tabcolor':'#4F4F4F',
+        'darkblu':'#0070E0',
+        'whitebgrgba':'rgba(250, 251, 252, 0.2);',
+        'acordionrgba':'rgba(4, 140, 185, 0.08)',
+        'grener':'rgba(60, 210, 165, 0.1)',
+        'brouner':'rgba(235, 87, 87, 0.1)',
+         'brounfed':' rgba(17, 66, 115, 0.04)',
+         'bengani':'#7223A6',
+         'dllstbbg':'#F8FAFB',
+        'broun' :' rgba(238, 190, 94, 0.2)',
+        'yellow' : '#F8B530',
+        'pentest' :'rgba(130, 157, 227, 0.15)',
+        'penfont' :'#829DE3',
+        'green':'#00A682',
+        'grey': '#EDF0F2',
+        'same':'rgba(111, 207, 151, 0.2)',
+
+
+
+
+
+
+
+
+
+      },
+      
+      fontSize: {
+        'sml':'13',
+        'sm': '14px',
+      },    
+      minHeight: {
+        'sm': '50px',
+        'calc': 'calc(100vh - 150px)', 
+      },
+      maxWidth: {
+        '2xl': '1530px',
+        'calc2': 'calc(100% - 440px)',
+        'lg': 'calc(100% - 120px)',
+      }, 
+      height: {
+        'xsh': 'calc(100% - 150px)',
+      }, 
+      tablewidth:{
+        'tbw':'16%',
+    },
+      screens: {
+        'xs' : '100px',
+        '2xls' : '1535px',
+       },   
+       backgroundImage:{
+         'bacgrediant':'linear-gradient(98.87deg, #114273 -2.57%, #57C09E 182.08%)',
+       },    
+    },
+   
+
+    fontFamily: { 
+      Poppins:['Poppins', 'sans-serif']
+    },
+  },
+  plugins: [ require('tw-elements/dist/plugin') ],
+}
